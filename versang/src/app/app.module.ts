cd5532197a4142da587b6df149cf1c7430c7519d
Damilola Michael Ige <idm.calculus@gmail.com>
2020-02-06 00:41:21 +0100

    soliciting for customers to join us at versa

--- versang/src/app/app.module.ts
+++ versang/src/app/app.module.ts
@@ -8,6 +8,7 @@ import { LandingPageComponent } from './landing-page/landing-page.component';
 import { WhyVersaComponent } from './why-versa/why-versa.component';
 import { InvestmentStepsComponent } from './investment-steps/investment-steps.component';
 import { VersaOffersComponent } from './versa-offers/versa-offers.component';
+import { JoinVersaComponent } from './join-versa/join-versa.component';
 
 
 @NgModule({
@@ -17,7 +18,8 @@ import { VersaOffersComponent } from './versa-offers/versa-offers.component';
     LandingPageComponent,
     WhyVersaComponent,
     InvestmentStepsComponent,
-    VersaOffersComponent
+    VersaOffersComponent,
+    JoinVersaComponent
   ],
   imports: [
     BrowserModule,
